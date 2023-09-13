d2813439edc01d7855b61d055556e73d719bb00c
Zubain Ahmad <thezubainahmad@gmail.com>
2023-09-13 20:27:43 +0530

    Project created

---
+++ src/main/java/com/example/campus_ease/CampusEaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.campus_ease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusEaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusEaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/campus_ease/CampusEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.campus_ease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusEaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
