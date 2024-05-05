fd6d62f8c4edde247d68d9710b4a297f95bf93c8
zubain86 <thezubainahmad@gmail.com>
2024-05-05 14:19:00 +0530

    CORS

--- src/main/java/com/example/campus_ease/CampusEaseApplication.java
+++ src/main/java/com/example/campus_ease/CampusEaseApplication.java
@@ -2,6 +2,10 @@ package com.example.campus_ease;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class CampusEaseApplication {
@@ -10,4 +14,20 @@ public class CampusEaseApplication {
 		SpringApplication.run(CampusEaseApplication.class, args);
 	}
 
+	@Bean
+	public CorsFilter corsFilter() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		final CorsConfiguration config = new CorsConfiguration();
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("OPTIONS");
+		config.addAllowedMethod("HEAD");
+		config.addAllowedMethod("GET");
+		config.addAllowedMethod("PUT");
+		config.addAllowedMethod("POST");
+		config.addAllowedMethod("DELETE");
+		config.addAllowedMethod("PATCH");
+		source.registerCorsConfiguration("/**",config);
+		return new CorsFilter(source);
+	}
 }
