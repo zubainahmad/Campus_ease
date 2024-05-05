d0895bb22f3fc3940ec9615e2bebd62da871c2fa
zubain86 <thezubainahmad@gmail.com>
2024-05-05 09:19:18 +0530

    Fix

--- src/main/java/com/example/campus_ease/response/JobResponse.java
+++ src/main/java/com/example/campus_ease/response/JobResponse.java
@@ -1,5 +1,6 @@
 package com.example.campus_ease.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,28 +11,28 @@ import lombok.Setter;
 @Getter
 @Setter
 public class JobResponse {
-
+    @JsonProperty("id")
     private Long id;
-
+    @JsonProperty("company_name")
     private String companyName;
-
+    @JsonProperty("job_description")
     private String jobDescription;
-
+    @JsonProperty("job_profile")
     private String jobProfile;
-
+    @JsonProperty("exp_ctc")
     private String expCTC;
-
+    @JsonProperty("reg_link")
     private String regLink;
-
+    @JsonProperty("file")
     private String file;
-
+    @JsonProperty("start_date")
     private String startDate;
-
+    @JsonProperty("end_date")
     private String endDate;
-
+    @JsonProperty("branches")
     private String branches;
-
+    @JsonProperty("job_location")
     private String jobLocation;
-
+    @JsonProperty("website_url")
     private String websiteUrl;
 }
